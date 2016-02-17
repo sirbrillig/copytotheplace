3f08092ef92b28a3e7ecbf7e3b0bd9268a68c5aa
Payton Swick <payton@foolord.com>
2016-02-17 12:27:02 -0500

    Add bin script
    Will not work yet because it requires the source to be transpiled first.

---
+++ bin/copytotheplace.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// External dependencies
+var parseArgs = require( 'minimist' );
+
+// Internal dependencies
+var copytotheplace = require( '../build/index' );
+
+var helpText = 'Utility to move files to a configurable and variable place.\n' +
+	'--place=<path>\tUse <path> as the destination directory.\n';
+
+// Parse command-line arguments
+var argv = parseArgs( process.argv.slice( 2 ), {
+	boolean: true
+} );
+if ( ! argv._.length ) {
+	console.log( helpText );
+	console.error( 'List me some files to copy.' );
+	process.exit( 1 );
+}
+
+copytotheplace.copytotheplace( argv._, argv.place );
