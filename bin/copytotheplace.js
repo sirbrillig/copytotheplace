6041460cf643cf0db039ce89bd5318a91c542387
Payton Swick <payton@foolord.com>
2016-02-18 10:09:48 -0500

    Update wording on project description

--- bin/copytotheplace.js
+++ bin/copytotheplace.js
@@ -6,7 +6,7 @@ var parseArgs = require( 'minimist' );
 // Internal dependencies
 var copytotheplace = require( '../build/index' );
 
-var helpText = 'copytotheplace is a utility to move files to a configurable and variable place.\n\n' +
+var helpText = 'copytotheplace is a utility to copy files to a configurable and variable place.\n\n' +
 	'Usage: copytotheplace [OPTIONS] <file1> [<file2>...]\n\n' +
 	'IMPORTANT NOTE: copytotheplace intentionally fails silently if no destination directory is set.\n' +
 	'You can set the destination directory on the command-line, via the COPYTOTHEPLACE environment\nvariable, or by writing COPYTOTHEPLACE=<some directory> to a .env file in this directory.\n\n' +
